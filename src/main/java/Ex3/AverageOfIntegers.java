59740fecf3feeeee9bb6dd419b0a6c77d6d3f7c8
George <symyon89@yahoo.com>
2021-11-24 18:00:38 +0200

    ok

--- src/main/java/Ex3/AverageOfIntegers.java
+++ src/main/java/Ex3/AverageOfIntegers.java
@@ -7,6 +7,7 @@ public class AverageOfIntegers implements Average<Integer>{
 
     @Override
     public Integer calculateAverage(List<Integer> integerList) {
+        //integerList.stream().mapToDouble().average().getAsDouble();
         return integerList.stream().reduce(0, Integer::sum)/integerList.size();
     }
 
